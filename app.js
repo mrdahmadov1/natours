9f0a550221257f4592940db6757bc76e34e0cbdc
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-06-19 04:14:12 +0400

    - user model - user routes - auth & user controllers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -7,6 +9,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
