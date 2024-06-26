029840cc0492548096909a8648da15c411118ee0
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-06-26 22:04:41 +0400

    - review model - review routes - review controller - global error handler - tour middlewares - review middlewares

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,33 +1,31 @@
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
-exports.getAll = (Model) =>
+exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    let filter = {};
-    // This tourId for Getting Tour's Reviews
-    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const doc = await Model.findByIdAndDelete(req.params.id);
 
-    const features = new APIFeatures(Model.find(filter), req.query);
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
 
-    const doc = await features.query;
-
-    res.status(200).json({
+    res.status(204).json({
       status: 'success',
-      results: doc.length,
-      data: {
-        data: doc,
-      },
+      data: null,
     });
   });
 
-exports.getOne = (Model, popOptions) =>
+exports.updateOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-    if (popOptions) query = query.populate(popOptions);
-    const doc = await query;
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-    if (!doc) return next(new AppError('No document found with that ID', 404));
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
 
     res.status(200).json({
       status: 'success',
@@ -49,14 +47,15 @@ exports.createOne = (Model) =>
     });
   });
 
-exports.updateOne = (Model) =>
+exports.getOne = (Model, popOptions) =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
 
-    if (!doc) return next(new AppError('No document found with that ID', 404));
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
 
     res.status(200).json({
       status: 'success',
@@ -66,14 +65,24 @@ exports.updateOne = (Model) =>
     });
   });
 
-exports.deleteOne = (Model) =>
+exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndDelete(req.params.id);
+    // To allow for nested GET reviews on tour (hack)
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
 
-    if (!doc) return next(new AppError('No document found with that ID', 404));
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
 
-    res.status(204).json({
+    res.status(200).json({
       status: 'success',
-      data: null,
+      results: doc.length,
+      data: {
+        data: doc,
+      },
     });
   });
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
 const User = require('./../../models/userModel');
-// const Review = require('./../../models/reviewModel');
+const Review = require('./../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -21,7 +21,7 @@ const importData = async () => {
   try {
     await Tour.create(tours);
     await User.create(users, { validateBeforeSave: false });
-    // await Review.create(reviews);
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -34,7 +34,7 @@ const deleteData = async () => {
   try {
     await Tour.deleteMany();
     await User.deleteMany();
-    // await Review.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
